7bf0c3ec81708756d1ef75359692cc1f5e9a05a7
Skyorder <skyorder.ck@gmail.com>
2019-04-23 19:54:02 +0200

    Initial commit

---
+++ PAUP_/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PAUP_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Onama()
+        {
+            ViewBag.Message = "Aplikacija za brzu online narudžbu hrane na području Međimurske županije.";
+
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            
+            ViewBag.Message = " Projekt izradili: ";
+
+            return View();
+        }
+    }
+}
---
+++ PAUP_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PAUP_.Startup))]
+namespace PAUP_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
